1fba9e7a7858711909c7ef29f976e2ff3739fb32
gguerrero07 <gguerrero@kippnyccp.org>
2016-05-24 12:36:09 -0400

    Update application.js

--- application.js
+++ application.js
@@ -2,18 +2,17 @@ $(document).ready(function(){
   $("#psych_description").css("display","none");
   $("#percussion_description").css("display","none");
   //when you mouse over the psych image, make the psych_display visible
-  $("#psych").mouseover(function(){
-    $("#percussion_description").css("display","none");
-    $("#psych_description").css("display","inline-block");
-  });
+  // $("#psych").mouseover(function(){
+  //   $("#percussion_description").css("display","none");
+  //   $("#psych_description").css("display","inline-block");
+  // });
   //when you mouse over the percussion image, make the percussion_display visible
   $("#percussion").mouseover(function(){
     $("#psych_description").css("display","none");
     $("#percussion_description").css("display","inline-block");
   });
-})
-$("#psych").click(function()) 
-  $("#psych").fadeIn("slow", function()) {
-    
+  $("#psych").click(function(){
+    $("#psych_description").fadeIn("slow", function() {
+    });
   });
-});
+})
