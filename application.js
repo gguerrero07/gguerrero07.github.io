d81091fa272c58e8fa7e4620c42a460ba9c77fbf
gguerrero07 <gguerrero@kippnyccp.org>
2016-05-24 12:17:30 -0400

    Update application.js

--- application.js
+++ application.js
@@ -1,1 +1,14 @@
-
+$(document)ready(function(){
+  $("#psych_description").css("display","none");
+  $("#percussion_description").css("display","none");
+  //when you mouse over the psych image, make the psych_display visible
+  $("#psych").mouseover(function(){
+    $("#percussion_description").css("display","none");
+    $("#psych_description").css("display","inline-block");
+  });
+  //when you mouse over the percussion image, make the percussion_display visible
+  $("#percussion").mouseover(function(){
+    $("#psych_description").css("display","none");
+    $("#percussion_description").css("display","inline-block");
+  });
+})
